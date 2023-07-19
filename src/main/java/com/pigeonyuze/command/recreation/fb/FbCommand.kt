c8b7dfeaf83acbb4388dcef501d594320b6ebdba
PigeonYuze <timothyfjxfa25@gmail.com>
2023-07-19 22:37:43 +0800

    fix: Can not run 'fb' command from consoles

--- src/main/java/com/pigeonyuze/command/recreation/fb/FbCommand.kt
+++ src/main/java/com/pigeonyuze/command/recreation/fb/FbCommand.kt
@@ -1,13 +1,12 @@
 package com.pigeonyuze.command.recreation.fb
 
 import com.pigeonyuze.MozeBotCore
-import com.pigeonyuze.util.from
-import com.pigeonyuze.util.quote
-import com.pigeonyuze.util.subject
+import com.pigeonyuze.util.*
 import net.mamoe.mirai.console.command.CommandContext
 import net.mamoe.mirai.console.command.SimpleCommand
 import net.mamoe.mirai.message.data.buildForwardMessage
 import net.mamoe.mirai.message.data.sendTo
+import net.mamoe.mirai.message.data.toPlainText
 import kotlin.random.Random
 
 object FbCommand: SimpleCommand(
@@ -16,8 +15,13 @@ object FbCommand: SimpleCommand(
     @Handler
     suspend fun handle(commandContext: CommandContext, who: String) = commandContext.run{
         val index = Random.nextInt(FbTextData.texts.size)
+        val randomObject = FbTextData.texts[index]
 
-        val random = "[MozeBot] 发病语录(#$index)\n" + FbTextData.texts[index].fb(who)
+        val random = "[MozeBot] 发病语录(#$index by ${randomObject.from})\n${randomObject.fb(who)}"
+        if (sender.isByConsole) {
+            sendMessage(random.toPlainText())
+            return@run
+        }
         if (random.length > 300) {
             buildForwardMessage(subject) {
                 from!! says random
